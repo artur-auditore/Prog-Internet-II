4de487b195c25067a866316dc5510ecf96a1063e
artur-auditore <defteros7@gmail.com>
2019-12-18 21:54:17 -0300

    hero-detail

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,9 +9,13 @@ import {HEROES} from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES
+  selectedHero: Hero;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
